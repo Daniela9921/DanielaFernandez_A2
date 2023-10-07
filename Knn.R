6e42ea197ef35ebfddda61e21369103edf12ca80
Daniela9921 <143049241+Daniela9921@users.noreply.github.com>
2023-10-07 18:54:40 -0500

    Update Knn.R

--- Knn.R
+++ Knn.R
@@ -1,77 +1,78 @@
+# Load necessary libraries
 library(tidyverse)
-library(ggplot2)
-
-# to get current script folder
-folder <- dirname(rstudioapi::getSourceEditorContext()$path)
-parentFolder <- dirname(folder)
-# Accessing by file address
-Social_Network_Ads <-
-  read_csv("~/GUERRA/Social_Network_Ads.csv")
-test_motion_data <- read_csv("~/GUERRA/Social_Network_Ads.csv")
-View(Social_Network_Ads)
-
-
-############### creacion de codigo xd
-
-# Instala y carga las bibliotecas necesarias
-if (!requireNamespace("caret", quietly = TRUE)) {
-  install.packages("caret")
-}
-if (!requireNamespace("e1071", quietly = TRUE)) {
-  install.packages("e1071")
-}
-
 library(caret)
-library(e1071)
+library(lattice)
 
-# Función para realizar kNN con diferentes normalizaciones
-perform_knn <- function(k_values, dataset) {
-  results <- list()
+# Load the dataset
+# Replace with the correct file path to your dataset
+diabetes_012_health_indicators_BRFSS2015 <- read_csv("~/GUERRA/diabetes_012_health_indicators_BRFSS2015.csv")
+
+# Sample 1% of the dataset for each class variable
+set.seed(123)  # For reproducibility
+sampled_data_diabetes <- diabetes_012_health_indicators_BRFSS2015 %>%
+  filter(Diabetes_012 == 0) %>%
+  sample_frac(0.01) %>%
+  bind_rows(diabetes_012_health_indicators_BRFSS2015 %>%
+              filter(Diabetes_012 == 1))
+
+sampled_data_heart_disease <- diabetes_012_health_indicators_BRFSS2015 %>%
+  filter(HeartDiseaseorAttack == 0) %>%
+  sample_frac(0.01) %>%
+  bind_rows(diabetes_012_health_indicators_BRFSS2015 %>%
+              filter(HeartDiseaseorAttack == 1))
+
+sampled_data_sex <- diabetes_012_health_indicators_BRFSS2015 %>%
+  filter(Sex == 0) %>%
+  sample_frac(0.01) %>%
+  bind_rows(diabetes_012_health_indicators_BRFSS2015 %>%
+              filter(Sex == 1))
+
+# Feature engineering (modify as needed)
+# You can scale numerical features, one-hot encode categorical variables, etc.
+
+# Function to train and evaluate KNN model
+train_evaluate_knn <- function(data, class_var_name) {
+  # Separate predictors and class variable
+  predictors <- select(data, -c(class_var_name))
+  class_column <- data[[class_var_name]]
   
-  # Convertir la columna Purchased en un factor
-  dataset$Purchased <- factor(dataset$Purchased, levels = c(0, 1), labels = c("No", "Sí"))
+  # Create a control set for K search
+  ctrl <- trainControl(method = "cv", number = 10)
   
-  for (k in k_values) {
-    # Sin normalización
-    knn_model <- train(Purchased ~ Age + EstimatedSalary, data = dataset, method = "knn", trControl = trainControl(method = "cv"), tuneGrid = data.frame(k = k))
-    knn_predictions <- predict(knn_model, newdata = dataset)
-    results[[paste("Original", k)]] <- knn_predictions
-    
-    # Normalización Min-Max
-    min_max_dataset <- as.data.frame(scale(dataset[, c("Age", "EstimatedSalary")]))
-    min_max_dataset$Purchased <- dataset$Purchased
-    knn_model_min_max <- train(Purchased ~ Age + EstimatedSalary, data = min_max_dataset, method = "knn", trControl = trainControl(method = "cv"), tuneGrid = data.frame(k = k))
-    knn_predictions_min_max <- predict(knn_model_min_max, newdata = min_max_dataset)
-    results[[paste("MinMax", k)]] <- knn_predictions_min_max
-  }
+  # Train a KNN model to find the optimal K
+  knn_model <- train(x = predictors,
+                     y = class_column,
+                     method = "knn",
+                     trControl = ctrl)
   
-  return(results)
+  # Print results
+  cat("KNN Model for", class_var_name, "\n")
+  print(knn_model)
 }
 
-# Cargar el conjunto de datos (reemplaza 'tu_archivo.csv' con el nombre de archivo real)
-data <- read.csv("~/GUERRA/Social_Network_Ads.csv")  # Ajusta la ruta según tu ubicación
+# Function to train and evaluate linear/multilinear regression models
+train_evaluate_linear_regression <- function(data, target_var_name) {
+  # Create a linear regression model with all variables
+  lm_model <- lm(reformulate(setdiff(names(data), target_var_name), response = target_var_name), data = data)
+  
+  # Print results
+  cat("Linear Regression Model for", target_var_name, "\n")
+  print(summary(lm_model))
+  
+  # Create a multilinear regression model with all variables
+  mlm_model <- lm(reformulate(names(data)[names(data) != target_var_name], response = target_var_name), data = data)
+  
+  # Print results
+  cat("Multilinear Regression Model for", target_var_name, "\n")
+  print(summary(mlm_model))
+}
 
-# Define los valores de k a probar
-k_values_to_test <- c(3, 5, 7)
+# Train and evaluate KNN models for each class variable
+train_evaluate_knn(sampled_data_diabetes, "Diabetes_012")
+train_evaluate_knn(sampled_data_heart_disease, "HeartDiseaseorAttack")
+train_evaluate_knn(sampled_data_sex, "Sex")
 
-# Obtener predicciones para los tres casos
-predictions <- perform_knn(k_values_to_test, data)
-
-# Obtener predicciones para los tres casos
-predictions <- perform_knn(k_values_to_test, data)
-
-# Gráfico de dispersión para las predicciones en el conjunto de entrenamiento (k=3)
-# Cambia "Original 3" por el caso y el valor de k que desees visualizar
-predicciones_k3 <- as.factor(predictions[["Original 3"]])
-
-# Agregar las predicciones al conjunto de datos original
-data$Predicciones <- predicciones_k3
-
-# Crear un gráfico de dispersión para visualizar las predicciones en el conjunto de entrenamiento
-ggplot(data, aes(x = Age, y = EstimatedSalary, color = Predicciones)) +
-  geom_point() +
-  scale_color_manual(values = c("No" = "red", "Sí" = "green")) +
-  labs(title = "Gráfico de Dispersión de Predicciones (k=3)",
-       x = "Edad", y = "Salario Estimado") +
-  theme_minimal()
-
+# Train and evaluate linear/multilinear regression models for each target variable
+train_evaluate_linear_regression(subset_bmi, "BMI")
+train_evaluate_linear_regression(subset_menthlth, "MentHlth")
+train_evaluate_linear_regression(subset_phys_hlth, "PhysHlth")
